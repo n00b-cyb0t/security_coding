57f7aa20b790fbfc76dcf66b3032acb12641fafb
n00b-cyb0t <98673630+n00b-cyb0t@users.noreply.github.com>
2022-02-22 21:47:59 -0500

    Create nmapscan.py

---
+++ nmapscan.py
@@ -0,0 +1,25 @@
+import nmap
+import json
+
+# this script needs to be run as root for certain arguements to work properly
+
+nm = nmap.PortScanner()
+
+def manualscan(ip, args):
+    result = [] 
+    try: 
+
+        # nmap --script=/usr/local/share/nmap/scripts/http-headers.nse 192.168.0.1
+        s = nm.scan(hosts=ip, arguments=args)
+        result.append(s)
+
+    except Exception as e: 
+        print("error: "+ str(e))
+    return result
+
+# working argument: '--script=/usr/local/share/nmap/scripts/http-headers.nse -p 21,22,80,443,137,139,3389'
+result = manualscan('192.168.0.1', '-sV -sS -p 21,22,80,443,137,139,3389')
+
+
+data = {"nmap_results": result}
+print(json.dumps(data, indent=3))
